2404a473145b35449aac6b1e8cb053330f58ae06
yami-five <konrad.rykala.94@gmail.com>
2019-01-28 08:36:52 +0100

    Half

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,5 +32,21 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grade in order to properly calculate a student's overall grade");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grade in order to properly calculate a student's overall grade");
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
